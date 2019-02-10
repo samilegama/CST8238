c766b8f16a5da2342732308fb180fedacf610ae6
Samuel Ebba <43682196+samilegama@users.noreply.github.com>
2019-02-10 09:55:39 -0500

    Add files via upload

---
+++ CST8238/Lab5/index.php
@@ -0,0 +1,18 @@
+
+<?php
+
+$firstName = "Samuel";
+$lastName = "Ebba";
+
+define("STUDENTMUNBER","040902847");
+echo $firstName." ".$lastName."<br>";
+echo "Student Number:".STUDENTMUNBER."<br>";
+echo "Hello World! "."I am using PHP!";
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "<a href='http://ebbaedu.com/CST8238/Assignment1/Index.html'>Assignment1</a>"."<br>";
+echo "<a href='http://ebbaedu.com/CST8238/Lab1/index.html'>Lab1</a>"."<br>";
+echo "<a href='http://ebbaedu.com/CST8238/Lab2/index.html'>Lab2</a>"."<br>";
+echo "<a href='http://ebbaedu.com/CST8238/Lab3/index.html'>Lab3</a>"."<br>";
+echo "<a href='http://ebbaedu.com/CST8238/Lab4/Index.html'>Lab4</a>"."<br>";
+
+?>
